82dc022977059228636c97fe870ccd1e474f6d0f
hlarsson <henrik@hlarsson.com>
2013-03-11 20:04:25 +0100

    Initial rails setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineSimulator::Application.config.secret_token = 'd6f28d967b2eb5b154c70d4cc88ddd0e039fa0a538d84a2160f197d5429faef0d0bcdd95d4a751233c9379efd7f1757843f930ebcb28d10809ee2a4bd50fa3d7'
